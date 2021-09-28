c2adda768f545f631e86dabeba116127e3a915c6
jeffkorby <82245490+jeffkorby@users.noreply.github.com>
2021-09-27 22:25:29 -0400

    heroku deploy

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect('mongodb://localhost/workout', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+
+
 
 const workoutSeed = [
   {
